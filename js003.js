2be68af6f5893b18bf2716f92a763a4b41a42469
Cherry.M <MengL@140.40.176.60.broad.hz.zj.dynamic.163data.com.cn>
2016-04-14 15:33:13 +0800

    simpleTrim

---
+++ js003.js
@@ -0,0 +1,28 @@
+/**
+ * Created by MengL on 16/4/14.
+ */
+function simpleTrim(s){
+    var len = s.length;
+    for(var i = 0;i<len;i++){
+        if(s[i]===' '||s[i]==='\t'){
+            //什么操作也不做,就是要找到第一个s[i]不是空格的i
+        }else {
+            break;//找到了,退出循环
+        }
+    }
+    if(i===len){
+        return'';//说明字符串是一系列空格或者tab
+    }
+    for(var j = len;j > 0;j--)//从右开始找不为空格的下标
+    {
+        if(s[j-1]===' '||s[j-1]==='\t'){
+
+        }
+        else {
+            break;
+        }
+    }
+    return s.substring(i,j);
+}
+var s=new String('   abcdef   ');
+alert(simpleTrim(s));
